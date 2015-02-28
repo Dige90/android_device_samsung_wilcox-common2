5c15a1d39a4fc72d894568579b01f2767a0cc6a1
Cristian <cristiandige90@gmail.com>
2015-02-28 16:23:49 +0100

    Update BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,36 +23,44 @@
 # inherit from common msm8930
 -include device/samsung/msm8930-common/BoardConfigCommon.mk
 
-TARGET_SPECIFIC_HEADER_PATH := device/samsung/serrano-common/include
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/wilcoxltexx/include
 
 # Kernel
 BOARD_KERNEL_CMDLINE         := androidboot.hardware=qcom user_debug=23 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_BASE            := 0x80200000
 BOARD_MKBOOTIMG_ARGS         := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE        := 2048
-TARGET_KERNEL_SOURCE         := kernel/samsung/msm8930-common
-ifneq ($(filter serranoltespr serranolteusc,$(TARGET_DEVICE)),)
-TARGET_KERNEL_CONFIG         := cyanogen_serrano_usa_defconfig
-else
-TARGET_KERNEL_CONFIG         := cyanogen_serrano_defconfig
-endif
+TARGET_KERNEL_SOURCE         := kernel/samsung/msm8930
+TARGET_KERNEL_CONFIG         := cyanogen_cane_defconfig
+TARGET_KERNEL_VARIANT_CONFIG := msm8930_cane_wilcox_eur_lte_defconfig
 TARGET_KERNEL_SELINUX_CONFIG := selinux_defconfig
 
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
+# Assert
+TARGET_OTA_ASSERT_DEVICE := wilcoxlte,wilcoxltexx,SM-G3815
+
 # Recovery
-TARGET_RECOVERY_FSTAB := device/samsung/serrano-common/rootdir/fstab.qcom
+TARGET_RECOVERY_FSTAB := device/samsung/wilcoxltexx/rootdir/fstab.qcom
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 
+# Partition sizes
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 10485760
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1572864000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 5821660160
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1761607680
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 5616139264
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/serrano-common/bluetooth
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/wilcoxltexx/bluetooth
+
+# Custom RIL class
+BOARD_RIL_CLASS := ../../../device/samsung/wilcoxltexx/ril/
+
+# NFC
+BOARD_HAVE_NFC := true
+#BOARD_NFC_CHIPSET := pn547
 
 # Audio
 AUDIO_FEATURE_ENABLED_INCALL_MUSIC := false
@@ -63,26 +71,24 @@ BOARD_USES_FLUENCE_INCALL := true
 BOARD_USES_FLUENCE_FOR_VOIP := true
 BOARD_USES_SEPERATED_AUDIO_INPUT := true
 BOARD_USES_SEPERATED_VOICE_SPEAKER := true
-BOARD_USES_SEPERATED_VOIP := true
 QCOM_CSDCLIENT_ENABLED := false
+
+# Use seperate devices for VOIP
+BOARD_USES_SEPERATED_VOIP := true
+
+# Charger
+BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_RES :=
 
 # Enable QCOM FM feature
 AUDIO_FEATURE_ENABLED_FM := true
 QCOM_FM_ENABLED := true
 BOARD_USES_SEPERATED_FM := true
 
-# Allow suspend in charge mode
-BOARD_CHARGER_ENABLE_SUSPEND := true
-
 # Camera
-TARGET_NEED_CAMERA_ZSL := true
-TARGET_NEED_FFC_PICTURE_FIXUP := true
-TARGET_NEED_FFC_VIDEO_FIXUP := true
+TARGET_NEED_DISABLE_AUTOFOCUS := true
 TARGET_NEED_DISABLE_FACE_DETECTION := true
 TARGET_NEED_DISABLE_FACE_DETECTION_BOTH_CAMERAS := true
 
 # Build our own PowerHAL
 TARGET_POWERHAL_VARIANT :=
-
-# Custom RIL class
-BOARD_RIL_CLASS := ../../../device/samsung/serrano-common/ril/
